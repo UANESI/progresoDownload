a1f58097ed58a4ecbabe3aa22b2fdb10486f43dc
tahelromero <tahel.romero@gmail.com>
2017-02-08 13:19:38 -0400

    prueba.php

--- prueba.php
+++ prueba.php
@@ -1,28 +1,14 @@
-<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
-<!--<link href="libs/css8/blitzer/jquery-ui-1.10.4.custom.min.css" rel="stylesheet">
-<link href="libs/css8/min.metroBeta.css" rel="stylesheet">
-<link href="libs/css8/metro-colors.min.css" rel="stylesheet">
-<link href="libs/css8/metro-icons.min.css" rel="stylesheet">
-<link href="libs/css8/autoComplete.css" rel="stylesheet">
-<link href="libs/css8/cleditor/jquery.cleditor.css" rel="stylesheet" type="text/css"/>
-<link href="libs/css8/datetimepicker/jquery.datetimepicker.css" rel="stylesheet">-->
+<title>Prueba de progresoDownload</title>
+<!--Se incorporan las librerias tanto de Jquery como la ajaxSetup.js-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script> window.jQuery || document.write('<script src="libs/js8/jquery-1.9.1.min.js"><\/script>') </script>
 <script src="libs/js8/ajaxSetup.js" type="text/javascript"></script>
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-<script> window.jQuery.ui || document.write('<script src="libs/js8/jquery-ui-1.10.4.custom.min.js"><\/script>') </script>
-<script src="libs/js8/select2/js/select2.min.js" type="text/javascript"></script>
-<script src="libs/js8/min.metroBeta2.js"></script>
-<script src="libs/js8/paramBas.js"></script>
-<script src="libs/js8/include.js" language="javascript"></script>
-<script src="libs/js8/moment.min.js" type="text/javascript"></script>
-<script src="libs/js8/paramAjax.js" type="text/javascript"></script>
-<script src="libs/js8/cleditor/jquery.cleditor.min.js" type="text/javascript"></script>
-<script src="libs/js8/datetimepicker/jquery.datetimepicker.js" type="text/javascript"></script>-->
+<!--Luego en el header se agrega el siguiente código básico
+Este ejemplo de uso descargará un archivo pdf y una imágen de forma simultánea, una vez que los descargue en el caso del pdf forzará al explorador a presentar el cuadro de diálogo de descarga, en el caso de la imágen la presentará en la página web)-->
+
 <script type="text/javascript">
 var pruebaDescarga 
 	$(document).ready(function(){
@@ -98,8 +84,6 @@ var pruebaDescarga
 			$("#progresoTXT"+xhr.opciones.id+"B").append("exitoSolicitud 2<br>");
 		}
 </script>
-</head>
-
 <body style="text-align:center">
 <input type="button" id="botonInicio" name="botonInicio" value="Iniciar descarga">
 <br>
